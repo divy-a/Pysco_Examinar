2d7327edccb257d541ff4d6025b515aef9feb2c0
divy-a <121391127+divy-a@users.noreply.github.com>
2023-04-30 20:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import traceback
 
 app = Flask(__name__)
 
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/')
 def index():
